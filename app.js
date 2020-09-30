79419dadc319b51a941bbfdedb6389705ca2ba19
JaimeAlb <jaimealbornoz2001@hotmail.com>
2020-09-30 16:16:49 -0300

    funcionalidad a botones completed

--- app.js
+++ app.js
@@ -2,10 +2,16 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
+const eraseButton = document.querySelector('.erase-button');
+const todoContainer = document.querySelector('.todo-container');
+const filterOption = document.querySelector('.filter-todo');
 
 // Event Listeners
 
 todoButton.addEventListener('click', addTodo);
+eraseButton.addEventListener('click', eraseTodo);
+todoList.addEventListener('click', deleteCheck);
+filterOption.addEventListener('click', filterTodo);
 
 // Functions
 
@@ -20,6 +26,8 @@ function addTodo(event){
     newTodo.innerText = todoInput.value;
     newTodo.classList.add('todo-item');
     todoDiv.appendChild(newTodo);
+    // ADD TODO TO LOCAL STORAGE
+    saveLocalTodos(todoInput.value);
     //CHECK MARK BUTTON
     const completedButton = document.createElement('button');
     completedButton.innerHTML = '<i class="fas fa-check"></i>';
@@ -34,4 +42,71 @@ function addTodo(event){
     todoList.appendChild(todoDiv);
     //CLEAR Todo INPUT VALUE
     todoInput.value = "";
+}
+function eraseTodo(event){
+    //Prevent form from submitting
+    event.preventDefault();
+    //crear boton
+    const botonManito = document.createElement('button');
+    botonManito.innerHTML = '<i class="far fa-thumbs-up"></i>';
+    botonManito.classList.add("boton-manito");
+
+    todoContainer.appendChild(botonManito);
+    
+}
+function deleteCheck(e){
+    const item = e.target;
+    //DELETE TODO
+    if(item.classList[0] === 'trash-btn'){
+        const todo = item.parentElement;
+        //Aniimation
+        todo.classList.add('fall');
+        todo.addEventListener('transitionend', function(){
+            todo.remove();
+        });
+        // todo.remove();
+    }
+    if(item.classList[0] === 'complete-btn'){
+        const todo = item.parentElement;
+        todo.classList.toggle('completed');
+    }
+}
+function filterTodo(e){
+    const todos = todoList.childNodes;
+    console.log(todos);
+    console.log(todos[0].classList.value);
+    todos.forEach(function(asd){
+        switch(e.target.value){
+            case "all":
+                asd.style.display = 'flex';
+                break;
+            case "completed":
+                if(asd.classList.contains('completed')){
+                    asd.style.display = 'flex';                    
+                }else{
+                    asd.style.display = 'none';
+                }
+                break;
+            case "uncompleted":
+                if(!asd.classList.contains('completed')){
+                    asd.style.display = 'flex';
+                }else{
+                    asd.style.display = 'none';
+                }
+                break;
+        }
+    });
+}
+
+function saveLocalTodos(todo){
+    console.log(todoInput.value)
+    // CHECK hey Do I already have something in there?
+    let todos;
+    if (localStorage.getItem("todos") === null){
+        todos = [];
+    }else{
+        todos = JSON.parse(localStorage.getItem("todos"));
+    }
+    todos.push(todo);
+    localStorage.setItem("todos", JSON.stringify(todos));
 }
